cb08d325f896603d9f3f6471f6e2f4985eb8efa6
zhufree <zhufree2013@gmail.com>
2015-09-29 16:26:13 +0800

    edit linklist

--- List/linklist.py
+++ List/linklist.py
@@ -12,9 +12,10 @@ class Node(object):
 		
 
 class LinkList(object):
-	"""docstring for LinkList"""
-	def __init__(self):
+	""" LinkList """
+	def __init__(self, datas):
 		self.head = Node(0)
+		self.create_by_tail(datas)
 
 	def __repr__(self):
 		p = self.head
@@ -25,6 +26,14 @@ class LinkList(object):
 			p = p.next
 		reprstr += ']'
 		return reprstr
+
+	def __len__(self):
+		length = 1
+		p = self.head
+		while p.next:
+			length += 1
+			p = p.next
+		return length
 
 	def create_by_head(self, datas):
 		self.clear()
@@ -46,10 +55,102 @@ class LinkList(object):
 	def clear(self):
 		self.head = Node(0)
 
+	def is_empty(self):
+		if len(self) == 0:
+			return True
+		else:
+			return False
+
+	def getData_by_index(self, index):
+		count = 0
+		p = self.head
+		while p.next:
+			if count == index:
+				return p.data
+			else:
+				p = p.next
+				count += 1
+		return False
+
+	def getData_by_value(self, value):
+		count = 0
+		p = self.head
+		while p.next:
+			if p.data == value:
+				return count
+			else:
+				p = p.next
+				count += 1
+		return False
+
+	def insertData(self, index, data):
+		count = 1
+		p = self.head
+		while p.next:
+			if count == index:
+				new_node = Node(data)
+				new_node.next = p.next
+				p.next = new_node
+				p = p.next
+				print self
+				return True
+			else:
+				p = p.next
+				count += 1
+		return False
+
+	def deleteData_by_index(self, index):
+		count = 1
+		p = self.head
+		while p.next:
+			if count == index:
+				p.next = p.next.next
+				p = p.next
+				print self
+				return True
+			else:
+				p = p.next
+				count += 1
+		return False
+
+	def deleteData_by_value(self, value):
+		count = 0
+		p = self.head
+		while p.next:
+			if p.next.data == value:
+				p.next = p.next.next
+				p = p.next
+				# print self
+				return True
+			else:
+				p = p.next
+				count += 1
+		return False
+
+	def delete_repeat(self):
+		q = self.head
+		while q.next:
+			p = q
+			while p.next:
+				if p.next.data != q.data:
+					p = p.next
+				else:
+					self.deleteData_by_value(q.data)
+					print 'delete:' + q.data
+					p = p.next
+			q = q.next
+
 if __name__ == '__main__':
-	llst = LinkList()
-	datas = [0, 2, 4, 6, 8]
-	llst.create_by_head(datas)
+	datas = ['a', 'b', 'c', 'd', 'e', 'a', 'c', 'g', 'd']
+	llst = LinkList(datas)
 	print llst
-	llst.create_by_tail(datas)
+	# llst.create_by_head(datas)
+	# print llst
+	# llst.create_by_tail(datas)
+	
+	# print len(llst)
+	# print llst.getData_by_index(3)
+	# llst.insertData(3, 'f')
+	# llst.deleteData_by_value('f')
+	llst.delete_repeat()
 	print llst
